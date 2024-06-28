f531a365f9af3ab210cec714a5352c14ad084764
nadiawaseem <161966596+Nadiawaseem@users.noreply.github.com>
2024-06-28 16:21:47 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+"use strict";
+// Check Grade using if,else condition
+let array = ["string", 1, true];
+console.log(["nadia", "20", "karachi"]);
---
+++ index.ts
@@ -0,0 +1,32 @@
+  // Check Grade using if,else condition
+
+
+
+
+  const percentage:number = 70;
+
+  if (percentage >=9 ){
+    console.log("A+");
+ }
+
+ // write a function that takes a string as input and returns the string with all vowels remved.
+
+  
+  let age =60;
+
+  if(age < 2){
+   console.log("the person is a babby.")
+  }
+    else if(age >=13&& age < 20){
+ console.log("the person is a kid.");
+}
+   else if(age >=20&& age <65){
+   console .log("the person is a adult.");}
+
+     //create a tuple to store a person's details (name, age, and address) and access its elements.
+
+    type arrayType= readonly[string,number,boolean];
+
+  let array: arrayType = ["string", 1, true];
+  console.log(["nadia","20","karachi"]);
+  
